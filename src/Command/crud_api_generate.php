46ddbcc528c4f7dfa0897263090dc6c722e72402
Samuel Besemuna Adjei <besemuna@gmail.com>
2019-06-21 17:55:54 +0000

    Added processFieldsFromFile Function

--- src/Command/crud_api_generate.php
+++ src/Command/crud_api_generate.php
@@ -3,6 +3,7 @@
 namespace Besemuna\LaravelApiCrudGenerator\Command;
 
 use Illuminate\Console\Command;
+use File;
 
 class GenerateCommand extends Command
 {
@@ -13,10 +14,7 @@ class GenerateCommand extends Command
      */
     protected $signature = 'laravel_crud:generate
                             {name : The name of the crud}
-                            {--fields= : Field names for controller and migration.}
-                            {--fields_from_file= : Field names from a json file .}
-                            {--foreign_key= : Foreign keys for migration.}
-                            {--foreign_key_from_file= : Foreign keys from a json file .}
+                            {--fields= : Field names from a json file .}
                             {--soft_deletes= : Allow for soft deletes .}';
 
     /**
@@ -25,6 +23,16 @@ class GenerateCommand extends Command
      * @var string
      */
     protected $description = 'This command generate crud endpoints on the fly!';
+
+    /** @var string  */
+    protected $schemaCreate;
+
+    /** @var string  */
+    protected $addValidation;
+
+    /** @var string  */
+    protected $updateValidation;
+
 
     /**
      * Create a new command instance.
@@ -43,6 +51,96 @@ class GenerateCommand extends Command
      */
     public function handle()
     {
-        //
+        # get name, model name, migration name, table name
+        $name = $this->argument('name');
+        $modelName = str_singular(ucfirst($name));
+        $migrationName = str_plural(snake_case($name));
+        $tableName = $migrationName;
+
+        # get and process fields and foreign keys
+        $fields = $this->processFieldsFromFile($this->option('fields'));
+
+
+        # set all info needed
+
+        # build model
+
+        # build controller
+
+        # build migration file
+
+        # add route file
+    }
+
+    /**
+     * Process fields from a file in a json format
+     * @param string $fileName Path of the file
+     * @return string
+     */
+    public function processFieldsFromFile($file) {
+        try {
+            $fieldsFile = File::get($file);
+            $json = json_decode($fieldsFile,true);
+
+            # prepare info for migration and validation
+            $validationArray = array();
+            $migrationArray = array();
+
+            foreach($json['fields'] as $key) {
+                # migration array
+                $array = [
+                    "name" => $key['name'],
+                    "type" => $key['type'],
+                    "modifier" => $key['modifier']
+                ];
+
+                if (isset($key['foreign_key'])) {
+                    $array['foreign_key'] = $key["foreign_key"];
+                }
+
+                array_push($migrationArray, $array);
+
+                # validation array
+                if (isset($key['validation'])) {
+                    $validationArray[$key["name"]] = $key["validation"];
+                }
+            }
+
+            $this->setValidationValues($validationArray);
+            $this->setMigrationValues($migrationArray);
+
+        }catch(\Illuminate\Contracts\Filesystem\FileNotFoundException $e) {
+            echo 'file does not exist';
+        }
+    }
+
+    /**
+     * Process and sets all values needed for final insertion
+     * @param array $validationArray Array of all info needed to set required values
+     * @return bool True if successful and false if there are errors
+     */
+    public function setValidationValues($validationArray) {
+        $addValidation = "";
+        foreach($validationArray as $key => $value) {
+            $addValidation .= "'{$key}' => '{$value}',\n";
+        }
+
+        # replace with double quotes and remove last comma
+        $addValidation = str_replace("'", '"', $addValidation);
+        $addValidation = rtrim($addValidation, ",");
+
+        $this->addValidation = $addValidation;
+
+        echo $this->addValidation;
+
+    }
+
+    /**
+     * Process and sets all values needed for final insertion
+     * @param array $migrationArray Array of all info needed to set required values
+     * @return bool
+     */
+    public function setMigrationValues($migrationArray) {
+
     }
 }
